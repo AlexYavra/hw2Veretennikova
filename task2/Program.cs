c47ec06740c7b6d03f5de1dd19c67488f0cb1b65
Lilia <liliqween12@gmail.com>
2023-06-27 10:27:09 +0300

    Задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ReadInt (string message)
+{
+    System.Console.Write(message);
+    string inputedStr = Console.ReadLine ();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = ReadInt("Введите число = ");
+if (ValidateNumber(number))
+{
+    Console.Write($"Третья цифра числа {number} равна {GetThirdRank(number)}");
+}
